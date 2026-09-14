b1f64a27b5107dae646e2d32c8a7669673d8661c
svc4p7vc8y-beep <svc4p7vc8y@privaterelay.appleid.com>
2026-09-14 15:44:20 +0300

    release: enable shared storage in calculator v133

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'eft-calculator-react-v132-roof-frame-1';
+const CACHE_NAME = 'eft-calculator-react-v133-team-storage-1';
 const APP_SHELL = ['./react.html', './manifest.webmanifest', './icons/eft-logo.png'];
 
 self.addEventListener('install', (event) => {
